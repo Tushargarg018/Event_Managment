d6e79c57c1d4dc5cad87be9b144805f7c1cee236
Aditya W <aditya.w@unthinkable.co>
2024-09-13 10:04:33 +0530

    Update EventBOfor Id

--- EM.Business/BOs/EventBO.cs
+++ EM.Business/BOs/EventBO.cs
@@ -10,6 +10,7 @@ namespace EM.Business.BOs
 {
     public class EventBO
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
