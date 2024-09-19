fdea2a191f1be8f01f78f24565fcfe37e466fd4f
Tushar Garg <tushar.garg@unthinkable.co>
2024-09-19 18:05:51 +0530

    added type_id in offerbo

--- EM.Business/BOs/OfferBO.cs
+++ EM.Business/BOs/OfferBO.cs
@@ -13,6 +13,8 @@ namespace EM.Business.BOs
         public int Id { get; set; }
         public int EventId { get; set; }
         public string? Type { get; set; }
+        [JsonPropertyName("type_id")]
+        public int TypeId { get; set; }
         public decimal Discount { get; set; }
         public int TotalOffers { get; set; }
         public int GroupSize { get; set; }
